219e9784161b3b56104c82be0630575e579757a7
julioDelacruzDU130073 <gintoky01@hotmail.com>
2014-11-13 11:26:45 -0600

    13/NOV/14

--- src/script.js
+++ src/script.js
@@ -3,11 +3,5 @@ var device = navigator.userAgent
 
 if (device.match(/Iphone/i)|| device.match(/Ipod/i)|| device.match(/Android/i)|| device.match(/J2ME/i)|| device.match(/HTC/i)) {
 window.location="http://localhost/proyecto/index.html";
-alert('Navegador movil')
-}
-else if (device.match(/Ipad/i))
-{
-alert('iPad');
-} else {
-alert('PC/Laptop/Mac');
+
 }
